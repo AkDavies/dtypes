7831ba75b44541c683b6a0a58d3bfb29ea8ff695
AkDavies <aod@andrew.cmu.edu>
2018-06-20 14:09:53 -0400

    Fixed test configuration.

--- tests/dtype_node_tests.R
+++ tests/dtype_node_tests.R
@@ -1,62 +1,62 @@
 context("Tests for the implementation of a node data type")
-source("../src/dtype_node.R")
+
 
 test_that(desc = "Test for the dtype_node constructor.",
-          simple_node <- dtype_node(id = 1),
-          expect_false(list(a = "A", b = "B") %>% type(), c("dtype_node", "dtype")), 
-          expect_equal(class(simple_node), c("dtype_node", "dtype")),
-          expect_error(non_numeric_id_node <- dtype_node(id = "Apples"),
-                       info = "The dtype_node constructor requires the argument for the node id to be a numeric type"),
-          expect_error(null_id_node <- dtype_node(id = NULL),
-                       info = "The dtype_node constructor requires the argument for the node id to be a numeric type"),
-          expect_equal(get_id(tenth_node),10),
-          
-          apple_node <- dtype_node(id = 1,
-                                   properties = list(property_names = c("name"),
-                                                     values = c("apple"))),
-          expect_equal(get_property(apple_node,"name"), "apple")
+          {
+              simple_node <- dtype_node(id = 1)
+              expect_false(typeof(list(a = "A", b = "B")) == c("dtype_node"))
+              expect_equal(class(simple_node)[1], c("dtype_node"))
+              expect_error(non_numeric_id_node <- dtype_node(id = "Apples"),
+                           info = "The dtype_node constructor requires the argument for the node id to be a numeric type")
+              expect_error(null_id_node <- dtype_node(id = NULL),
+                           info = "The dtype_node constructor requires the argument for the node id to be a numeric type")
+          }
 )
 
 test_that(desc = "Tests for get_id",
-          third_node <- dtype_node(id = 3),
-          expect_type(get_id(third_node), "numeric"),
-          expect_equal(get_id(third_node), 3)
+          {
+              third_node <- dtype_node(id = 3)
+              expect_type(get_id(third_node), "double")
+              expect_equal(get_id(third_node), 3)
+              tenth_node <- dtype_node(id = 10)
+              expect_equal(get_id(tenth_node),10)
+          }
 )
 
 test_that(desc = "Tests for get_properties",
-          tenth_node <- dtype_node(id = 10),
-          expect_type(get_properties(tenth_node), "list"),
-          expect_is(get_properties(tenth_node),NULL),
-          shiny_banana_bunch <- dtype_node(id = 1,
-                                            properties = sbb_properties <- list(property_names = c("name",
-                                                                                                   "color",
-                                                                                                   "shape",
-                                                                                                   "number"),
-                                                                                values = c("banana",
-                                                                                           "yellow",
-                                                                                           "curved",
-                                                                                           "six")
-                                                                            )
-                                ),
-          expect_equal(get_properties(shiny_banana_bunch), sbb_properties)
+          {
+              tenth_node <- dtype_node(id = 10)
+              expect_type(get_properties(tenth_node), "list")
+              expect_equal(length(get_properties(tenth_node)), 0)
+              shiny_banana_bunch <- dtype_node(id = 1,
+                                                properties = sbb_properties <- list(name = "banana",
+                                                                                    color = "yellow",
+                                                                                    shape = "curved",
+                                                                                    number = 6
+                                                                                )
+                                    )
+              expect_equal(get_properties(shiny_banana_bunch), sbb_properties)
+          }
 )
 
 test_that(desc = "Tests for get_property",
-          shiny_banana_bunch <- dtype_node(id = 1,
-                                            properties = sbb_properties <- list(property_names = c("name",
-                                                                                                   "color",
-                                                                                                   "shape",
-                                                                                                   "number"),
-                                                                                values = c("banana",
-                                                                                           "yellow",
-                                                                                           "curved",
-                                                                                           "six")
-                                                                            )
-                                ),
-          expect_equal(get_property(shiny_banana_bunch,"name"), "banana"),
-          expect_equal(get_property(shiny_banana_bunch,"color"), "yellow"),
-          expect_equal(get_property(shiny_banana_bunch,"shape"), "curved"),
-          expect_equal(get_property(shiny_banana_bunch,"number"), "six")
+          {
+              apple_node <- dtype_node(id = 1,
+                                       properties = list(name = "apple")
+              )
+              expect_equal(get_property(apple_node,"name"), "apple")
+              shiny_banana_bunch <- dtype_node(id = 1,
+                                                properties = sbb_properties <- list(name = "banana",
+                                                                                    color = "yellow",
+                                                                                    shape = "curved",
+                                                                                    number = 6
+                                                                                )
+                                    )
+              expect_equal(get_property(shiny_banana_bunch,"name"), "banana")
+              expect_equal(get_property(shiny_banana_bunch,"color"), "yellow")
+              expect_equal(get_property(shiny_banana_bunch,"shape"), "curved")
+              expect_equal(get_property(shiny_banana_bunch,"number"), 6)
+          }
 )
 
 
